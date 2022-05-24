5606964d6778fe4d048230fcf069105613718a40
kmyeasin <kmyeasin29>
2022-05-25 01:14:34 +0600

    Database connected

--- index.js
+++ index.js
@@ -1,11 +1,35 @@
-const express = require('express')
+const express = require('express');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const cors = require('cors');
+const { get } = require('express/lib/response');
 const app = express();
 require('dotenv').config()
 const port = process.env.PORT || 5000;
 
 app.use(cors())
-app.use(express.json())
+app.use(express.json());
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ciaiz.mongodb.net/?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run(){
+    try{
+        await client.connect();
+        const productCollection=client.db('tools').collection('services');
+        app.get('/service', async(req, res)=>{
+            const query={};
+            const cursor = productCollection.find(query);
+            const services=await cursor.toArray();
+            res.send(services);
+        })
+    }
+    finally{
+
+    }
+}
+run().catch(console.dir)
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
