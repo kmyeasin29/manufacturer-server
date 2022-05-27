6624dee4da75b03d32a5d24b691aedc18e7f0579
kmyeasin <kmyeasin29>
2022-05-28 00:54:34 +0600

    port corection

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const cors = require('cors');
 const { get } = require('express/lib/response');
 const app = express();
 require('dotenv').config()
-const port = process.env.PORT || 4000;
+const port = process.env.PORT || 5000;
 
 app.use(cors())
 app.use(express.json());
